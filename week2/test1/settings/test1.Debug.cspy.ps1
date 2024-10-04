83d57d65ccfeacfd8c38647db40ef9b48a8bd43c
Sjaeyeong <duddl705@naver.com>
2024-10-04 22:18:59 +0900

    3주차최종본

--- week2/test1/settings/test1.Debug.cspy.ps1
+++ week2/test1/settings/test1.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\user\Desktop\test1\settings\test1.Debug.general.xcl" --backend -f "C:\Users\user\Desktop\test1\settings\test1.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu8\Documents\GitHub\PNU_Embedded_System_Design_Lab\week2\test1\settings\test1.Debug.general.xcl" --backend -f "C:\Users\pnu8\Documents\GitHub\PNU_Embedded_System_Design_Lab\week2\test1\settings\test1.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\user\Desktop\test1\settings\test1.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\user\Desktop\test1\settings\test1.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\pnu8\Documents\GitHub\PNU_Embedded_System_Design_Lab\week2\test1\settings\test1.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\pnu8\Documents\GitHub\PNU_Embedded_System_Design_Lab\week2\test1\settings\test1.Debug.driver.xcl" 
 }
